6c1869ddd74ae2dee04122eea4b2467d1b009ef8
Shahed Noor <shahednoor28@gmail.com>
2024-10-15 23:29:19 +0600

    Carousel slider added for project section

--- lib/sections/projects_section.dart
+++ lib/sections/projects_section.dart
@@ -31,6 +31,33 @@ class _ProjectsSectionState extends State<ProjectsSection> {
     }
   }
 
+  Size adjustButtonSize(double width) {
+    return Size(
+      width >= 1200
+          ? 150
+          : width > 999
+              ? width / 90
+              : width > 900
+                  ? width / 80
+                  : width > 700
+                      ? width / 70
+                      : width > 600
+                          ? width / 60
+                          : width / 50,
+      width >= 1200
+          ? 50
+          : width > 999
+              ? width / 22
+              : width > 900
+                  ? width / 21
+                  : width > 700
+                      ? width / 19
+                      : width > 600
+                          ? width / 17
+                          : width / 15,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
@@ -77,11 +104,13 @@ class _ProjectsSectionState extends State<ProjectsSection> {
               ),
             ],
           ),
-          const SizedBox(height: 20,),
+          const SizedBox(
+            height: 20,
+          ),
           Center(
             child: CarouselSlider.builder(
               options: CarouselOptions(
-                aspectRatio: 16/9,
+                aspectRatio: 16 / 9,
                 viewportFraction: 0.8,
                 initialPage: 0,
                 enableInfiniteScroll: true,
@@ -96,10 +125,113 @@ class _ProjectsSectionState extends State<ProjectsSection> {
               ),
               itemCount: 5,
               itemBuilder: (BuildContext context, int index, int realIndex) {
-                return Container(
-                  width: MediaQuery.sizeOf(context).width,
-                  margin: const EdgeInsets.symmetric(horizontal: 5.0),
-                  child: Image.asset("assets/images/projects_image/social_wall.png"),
+                return Stack(
+                  children: [
+                    // The Image
+                    Container(
+                      width: MediaQuery.sizeOf(context).width,
+                      margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                      child: Image.asset(
+                        "assets/images/projects_image/social_wall.png",
+                        fit: BoxFit
+                            .cover, // Ensures the image covers the container
+                      ),
+                    ),
+
+                    // The new button above the row
+                    Positioned(
+                      bottom: width < 700
+                          ? width * 0.17
+                          : width < 900
+                              ? width * 0.16
+                              : width < 1000
+                                  ? width * 0.15
+                                  : width * 0.14,
+                      // Position above the existing row
+                      right: MediaQuery.sizeOf(context).width *
+                          0.02, // Responsive position
+                      child: Center(
+                        child: TextButton.icon(
+                          onPressed: () {},
+                          icon: FaIcon(
+                            FontAwesomeIcons.download,
+                            color: AppColor.whitePrimary,
+                            size: width / 50,
+                          ),
+                          label: Text(
+                            "Download Release",
+                            style: TextStyle(
+                              color: AppColor.whitePrimary,
+                              fontSize: width / 50,
+                            ),
+                          ),
+                          style: TextButton.styleFrom(
+                            backgroundColor: AppColor.backgroundSecondary,
+                            minimumSize: adjustButtonSize(width),
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    // The Button overlay with two buttons in a Row
+                    Positioned(
+                      bottom: MediaQuery.sizeOf(context).width *
+                          0.10, // Responsive position
+                      right: MediaQuery.sizeOf(context).width *
+                          0.02, // Responsive position
+                      child: Row(
+                        children: [
+                          // First button
+                          Center(
+                            child: TextButton.icon(
+                              onPressed: () {},
+                              icon: FaIcon(
+                                FontAwesomeIcons.lightbulb,
+                                color: AppColor.whitePrimary,
+                                size: width / 50,
+                              ),
+                              label: Text(
+                                "Live",
+                                style: TextStyle(
+                                  color: AppColor.whitePrimary,
+                                  fontSize: width / 50,
+                                ),
+                              ),
+                              style: TextButton.styleFrom(
+                                backgroundColor: AppColor.backgroundSecondary,
+                                minimumSize: adjustButtonSize(width),
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                              width: 10), // Space between the buttons
+
+                          // Second button
+                          Center(
+                            child: TextButton.icon(
+                              onPressed: () {},
+                              icon: FaIcon(
+                                FontAwesomeIcons.github,
+                                color: AppColor.whitePrimary,
+                                size: width / 50,
+                              ),
+                              label: Text(
+                                "Code",
+                                style: TextStyle(
+                                  color: AppColor.whitePrimary,
+                                  fontSize: width / 50,
+                                ),
+                              ),
+                              style: TextButton.styleFrom(
+                                backgroundColor: AppColor.backgroundSecondary,
+                                minimumSize: adjustButtonSize(width),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 );
               },
             ),
